6514db810fdd107af2109a4ce4450e2279a0c0c6
Avanish Singh <1781464@tcs.com>
2023-09-05 14:36:29 +0200

    testing push

--- Online_judge/Online_judge/settings.py
+++ Online_judge/Online_judge/settings.py
@@ -12,12 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-lij#r)%pcrw=*w2$oaxyj2fyil!#s=ykxulw^p1fbcofzxnpu'
